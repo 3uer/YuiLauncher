7349bb64f74ccfd44304c1885771df728e1421cf
3uer <vania.vaniacoper@gmail.com>
2026-03-28 23:46:24 +0100

    Fix: Restored minecraft dependencies and added missing color

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -10,24 +10,73 @@ android {
 
     defaultConfig {
         minSdk = 26
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    packaging {
+        resources {
+            excludes += listOf(
+                "META-INF/AL2.0",
+                "META-INF/LGPL2.1",
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/license.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/notice.txt",
+                "META-INF/ASL2.0",
+                "META-INF/INDEX.LIST",
+                "META-INF/*.kotlin_module"
+            )
+        }
+    }
 }
 
-// НОВЫЙ И ИДЕАЛЬНЫЙ СПОСОБ ДЛЯ KOTLIN
 kotlin {
     jvmToolchain(17)
 }
 
 dependencies {
     implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.sqlite.framework)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.annotations)
+    implementation(libs.androidx.annotation)
     implementation(libs.okhttp)
+    implementation(libs.httpclient)
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.iid)
+    implementation(libs.pkix)
     implementation(libs.gson)
-    implementation(libs.material)
+    implementation(libs.simple.xml)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.browser)
+    implementation(libs.core)
+    implementation(libs.prov)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.billing)
+    implementation(libs.conscrypt.android)
+    implementation(libs.androidx.games.activity)
+    implementation(libs.core.splashscreen)
+    implementation(libs.androidx.preference.ktx)
     implementation(libs.appcompat)
+    implementation(libs.material)
 }
